6797f3c7f9ed8c6fe3bf41d605de70edcdc9e73b
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-17 10:29:33 +0200

    implement get all posts

--- src/resolvers/Query/Query.ts
+++ src/resolvers/Query/Query.ts
@@ -2,5 +2,17 @@
 export const Query = {
     users: async (parent: any, args: any, { prisma }: any) => {
         return await prisma.user.findMany()
+    },
+    posts: async (parent: any, args: any, { prisma }: any) => {
+        return await prisma.post.findMany({
+            where: {
+                published: true
+            },
+            orderBy: [
+                {
+                    createdAt: 'desc'
+                }
+            ]
+        })
     }
 }
