fcb54c28418761d4eb86e2ef8ab461af3f5689d1
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-17 10:12:47 +0200

    implement update post

--- src/resolvers/Mutation/post.ts
+++ src/resolvers/Mutation/post.ts
@@ -18,5 +18,44 @@ export const postResolvers = {
             userError: null,
             post: newPost
         }
-    }
+    },
+    updatePost: async (parent: any, args: { id: number, post: { title: string, content: string } }, { prisma, userInfo }: any, info: any) => {
+        const userId = userInfo?.userId;
+        if (!userId) {
+            return {
+                userError: 'Unauthorized',
+                post: null
+            }
+        }
+        const post = await prisma.post.findFirst({
+            where: {
+                id: args.id
+            }
+        });
+        if (!post) {
+            return {
+                userError: 'Post not found',
+                post: null
+            }
+        }
+        if (post.authorId !== userId) {
+            return {
+                userError: 'Unauthorized',
+                post: null
+            }
+        }
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: args.id
+            },
+            data: {
+                title: args.post.title,
+                content: args.post.content
+            }
+        });
+        return {
+            userError: null,
+            post: updatedPost
+        }
+    },
 }
