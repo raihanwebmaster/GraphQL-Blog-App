d080c8821449393ec617cb164992370fd94cd00a
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-18 15:31:15 +0200

    update

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -74,7 +74,7 @@ const Signup = () => {
     return (
         <section class="bg-gray-50 dark:bg-gray-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <a onClick={() =>navigate('/')} class="cursor-pointer flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                     <BanknotesIcon className="w-8 h-8 mr-2" />
                     GraphQL
                 </a>
