96346df947450d158ae15e00583f4f828f1a74f2
raihanwebmaster <raihanemon2015@gmail.com>
2024-06-19 11:54:38 +0200

    update

--- client/src/pages/Post/PostCard.js
+++ client/src/pages/Post/PostCard.js
@@ -1,27 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import './PostCard.css'
+import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 const PostCard = ({ post }) => {
 
     const [formatedDate, setFormatedDate] = useState("");
-
     useEffect(() => {
         if (post.createdAt) {
-            let date = post.createdAt;
-            date /= 1000
-            const dateInstance = new Date(date)
-            setFormatedDate(`${dateInstance.getDate()}-${dateInstance.getMonth() + 1}-${dateInstance.getFullYear()}`)
+            const dateInstance = new Date(Number(post.createdAt)); // Ensure the timestamp is a number
+            setFormatedDate(`${dateInstance.getDate()}-${dateInstance.getMonth() + 1}-${dateInstance.getFullYear()}`);
         }
-    }, [post])
+    }, [post]);
+
     return (
-        <div className='border w-96 m-4 p-4 rounded-md border-black-400'>
-            <h3 className='text-lg font-bold'>{post.title}</h3>
-            <div className='text-state-500 text-sm text-gray-700 flex justify-between py-2'>
-                <p>Author Name: {post.author.name}</p>
-                <p>Date: {formatedDate}</p>
+        <article className="flex max-w-xl flex-col items-start justify-between border w-96 m-4 p-4 rounded-md border-black-400">
+            <div className="flex items-center gap-x-4 text-xs">
+                <h2 className="text-gray-500">
+                    {formatedDate}
+                </h2>
             </div>
-            <p className='text-sm'>{post.content}</p>
-        </div>
+            <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post?.href}>
+                        <span className="absolute inset-0" />
+                        {post.title}
+                    </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post?.content}</p>
+            </div>
+            <div className="relative mt-8 flex items-center gap-x-4">
+                <UserCircleIcon className="w-8 h-8" />
+                <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">
+                        <a href={post?.author?.href}>
+                            <span className="absolute inset-0" />
+                            {post?.author?.name}
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </article>
+
     );
 };
 
--- client/src/pages/Post/Posts.js
+++ client/src/pages/Post/Posts.js
@@ -22,16 +22,22 @@ const Posts = () => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
-    // console.log(data.posts)
-
     return (
-        <div>
-            <h1 className='text-center font-bold text-5xl my-4 pb-4'>Posts</h1>
-            <hr />
-            <div className='flex flex-wrap'>
-                {
-                    data.posts.map((post) => <PostCard post={post} />)
-                }
+        <div className="bg-white py-24 sm:py-32 h-screen">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                    <p className="mt-2 text-lg leading-8 text-gray-600">
+                        Learn how to grow your business with our expert advice.
+                    </p>
+                </div>
+                <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    {
+                        data.posts.length > 0 ? data.posts.map((post, index) => (
+                            <PostCard key={index} post={post} />
+                        )) : <p>No post available</p>
+                    }
+                </div>
             </div>
         </div>
     );
